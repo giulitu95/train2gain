ce15eb1734adfc4e8172a3c326c83d37d77335d4
Alessio Paternoster <AlessioPaternoster@users.noreply.github.com>
2018-08-27 10:23:41 +0200

    Add some athlete / trainer sync code

--- Android/train2gain/app/src/main/java/com/train2gain/train2gain/service/SyncDataTask.java
+++ Android/train2gain/app/src/main/java/com/train2gain/train2gain/service/SyncDataTask.java
@@ -6,12 +6,21 @@ import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 import com.train2gain.train2gain.model.entity.Athlete;
+import com.train2gain.train2gain.model.entity.Schedule;
+import com.train2gain.train2gain.model.entity.ScheduleLoad;
+import com.train2gain.train2gain.model.entity.ScheduleNote;
 import com.train2gain.train2gain.model.entity.Trainer;
 import com.train2gain.train2gain.model.entity.User;
 import com.train2gain.train2gain.repository.AthleteRepository;
+import com.train2gain.train2gain.repository.ScheduleLoadRepository;
+import com.train2gain.train2gain.repository.ScheduleNoteRepository;
+import com.train2gain.train2gain.repository.ScheduleRepository;
 import com.train2gain.train2gain.repository.TrainerRepository;
 import com.train2gain.train2gain.repository.UserRepository;
 import com.train2gain.train2gain.repository.common.Resource;
+
+import java.util.Date;
+import java.util.List;
 
 public class SyncDataTask extends AsyncTask<Long, Void, Boolean> {
 
@@ -27,6 +36,9 @@ public class SyncDataTask extends AsyncTask<Long, Void, Boolean> {
     private final UserRepository userRepository;
     private final AthleteRepository athleteRepository;
     private final TrainerRepository trainerRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final ScheduleLoadRepository scheduleLoadRepository;
+    private final ScheduleNoteRepository scheduleNoteRepository;
 
     private final CallbackInterface callbackImpl;
 
@@ -34,6 +46,9 @@ public class SyncDataTask extends AsyncTask<Long, Void, Boolean> {
         this.userRepository = UserRepository.getInstance(context);
         this.athleteRepository = AthleteRepository.getInstance(context);
         this.trainerRepository = TrainerRepository.getInstance(context);
+        this.scheduleRepository = ScheduleRepository.getInstance(context);
+        this.scheduleLoadRepository = ScheduleLoadRepository.getInstance(context);
+        this.scheduleNoteRepository = ScheduleNoteRepository.getInstance(context);
         this.callbackImpl = interfaceImpl;
     }
 
@@ -50,11 +65,15 @@ public class SyncDataTask extends AsyncTask<Long, Void, Boolean> {
         User userObject = userLiveDataResource.getValue().getData();
         switch(userObject.getType()){
             case ATHLETE:
-                update = updateAthleteInfo(userObject);
+                update = updateInfoForAthlete(userObject);
                 break;
             case TRAINER:
+                update = updateInfoForTrainer(userObject);
                 break;
             case BOTH:
+                boolean update1 = updateInfoForAthlete(userObject);
+                boolean update2 = updateInfoForTrainer(userObject);
+                update = update1 && update2;
                 break;
         }
 
@@ -71,19 +90,23 @@ public class SyncDataTask extends AsyncTask<Long, Void, Boolean> {
     }
 
     /**
-     * Synchronizes the athlete info
-     * @param userObject
-     * @return
+     * Called to synchronize, with the web server, all the athlete's information like athlete details,
+     * schedules, training history, ecc..
+     * @param athleteUserObject the athlete's user for which we want to synchronize the information
+     * @return true, if the synchronization has been successful
+     *         false otherwise
      */
-    private boolean updateAthleteInfo(User userObject){
+    private boolean updateInfoForAthlete(User athleteUserObject){
         // Sync athlete info
-        LiveData<Resource<Athlete>> athleteLiveDataResource = this.athleteRepository.updateAthlete(userObject.getId());
+        LiveData<Resource<Athlete>> athleteLiveDataResource = this.athleteRepository.updateAthlete(athleteUserObject.getId());
         if(!genericDataSync(athleteLiveDataResource)){
             return false;
         }
 
-        // Sync trainer user info
         long trainerUserId = athleteLiveDataResource.getValue().getData().getTrainerUserId();
+        long athleteId = athleteLiveDataResource.getValue().getData().getUserId();
+
+        // Sync trainer user info
         LiveData<Resource<User>> trainerUserLiveDataResource = this.userRepository.updateUser(trainerUserId);
         if(!genericDataSync(trainerUserLiveDataResource)){
             return false;
@@ -95,23 +118,65 @@ public class SyncDataTask extends AsyncTask<Long, Void, Boolean> {
             return false;
         }
 
-        // TODO Sync current schedule
+        // Sync schedule info
+        LiveData<Resource<Schedule>> scheduleLiveDataResource = this.scheduleRepository.updateScheduleByAthleteUserId(athleteId);
+        if(!genericDataSync(scheduleLiveDataResource)){
+            // No schedule, ok, continue..
+            return true;
+        }
+
+        long scheduleId = scheduleLiveDataResource.getValue().getData().getId();
+
+        // Sync schedule loads
+        LiveData<Resource<List<ScheduleLoad>>> scheduleLoadsListLiveDataResource = this.scheduleLoadRepository.updatedScheduleLoads(scheduleId, athleteId, new Date(0));
+        if(!genericDataSync(scheduleLoadsListLiveDataResource)){
+            return false;
+        }
+
+        // Sync schedule notes
+        LiveData<Resource<List<ScheduleNote>>> scheduleNotesListLiveDataResource = this.scheduleNoteRepository.updateScheduleNotes(scheduleId, athleteId, new Date(0));
+        if(!genericDataSync(scheduleNotesListLiveDataResource)){
+            return false;
+        }
+
+        // TODO Sync training history & Sync all the uploads
         return true;
     }
 
     /**
-     * Generic data sync handler
-     * @param resourceLiveData
-     * @param <DataType>
-     * @return
+     * Called to synchronize, with the web server, all the trainer's information like trainer details,
+     * schedules that he/she created, notes added to the schedules, ecc..
+     * @param trainerUserObject the trainer's user for which we want to synchronize the information
+     * @return true, if the synchronization has been successful
+     *         false otherwise
+     */
+    private boolean updateInfoForTrainer(User trainerUserObject){
+        long trainerUserId = trainerUserObject.getId();
+
+        // Sync trainer info
+        LiveData<Resource<Trainer>> trainerLiveDataResource = this.trainerRepository.updateTrainer(trainerUserId);
+        if(!genericDataSync(trainerLiveDataResource)){
+            return false;
+        }
+
+        // TODO Sync all the uploads
+        return true;
+    }
+
+    /**
+     * A generic data sync handler. It is use to observe the data and take decision base on data's state
+     * @param resourceLiveData the data that we want to sync with the web server
+     * @param <DataType> type of data we want to sync (it can be an Entity or a List)
+     * @return true, if sync has been handled correctly
+     *         false otherwise
      */
     private<DataType> boolean genericDataSync(LiveData<Resource<DataType>> resourceLiveData){
         resourceLiveData.observeForever(dataTypeResource -> {});
         while(resourceLiveData.getValue() == null || resourceLiveData.getValue().getStatus() == Resource.Status.LOADING);
-        if(resourceLiveData.getValue().getData() == null){
-            return false;
-        }else{
+        if(resourceLiveData.getValue().getData() != null){
             return true;
+        }else{
+            return false;
         }
     }
 
