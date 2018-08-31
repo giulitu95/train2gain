35bedeba2e0887bbca49cf852ab7ac07657be792
Alessio Paternoster <AlessioPaternoster@users.noreply.github.com>
2018-08-31 14:50:31 +0200

    Init DailyWorkout fragment and layout

---
+++ Android/train2gain/app/src/main/java/com/train2gain/train2gain/ui/fragment/athlete/DailyWorkoutFragment.java
@@ -0,0 +1,57 @@
+package com.train2gain.train2gain.ui.fragment.athlete;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.train2gain.train2gain.R;
+
+public class DailyWorkoutFragment extends Fragment {
+
+    private final static String ATHLETE_USER_ID_KEY = "ATHLETE_USER_ID_KEY";
+    private long athleteUserId = -1;
+
+    public static DailyWorkoutFragment newInstance(final long athleteUserId){
+        DailyWorkoutFragment dailyWorkoutFragment = new DailyWorkoutFragment();
+        Bundle fragmentArgs = new Bundle();
+        fragmentArgs.putLong(DailyWorkoutFragment.ATHLETE_USER_ID_KEY, athleteUserId);
+        dailyWorkoutFragment.setArguments(fragmentArgs);
+        return dailyWorkoutFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get fragment argument (athlete user id)
+        Bundle fragmentParams = getArguments();
+        if(fragmentParams != null && fragmentParams.containsKey(DailyWorkoutFragment.ATHLETE_USER_ID_KEY)){
+            this.athleteUserId = (Long) fragmentParams.getLong(DailyWorkoutFragment.ATHLETE_USER_ID_KEY);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recycler_view_daily_workout_item_section, parent, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        // TODO add view model managing
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().setTitle(getString(R.string.daily_workout_title));
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Nothing to do
+    }
+
+}
