65051bd05ecd577f543cd81702e05f7ef4832ce8
Alessio Paternoster <AlessioPaternoster@users.noreply.github.com>
2018-09-01 14:53:18 +0200

    DailyWorkoutFragment: add code to handle recycler view

--- Android/train2gain/app/src/main/java/com/train2gain/train2gain/ui/fragment/athlete/DailyWorkoutFragment.java
+++ Android/train2gain/app/src/main/java/com/train2gain/train2gain/ui/fragment/athlete/DailyWorkoutFragment.java
@@ -1,17 +1,32 @@
 package com.train2gain.train2gain.ui.fragment.athlete;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.train2gain.train2gain.R;
+import com.train2gain.train2gain.adapter.DailyWorkoutRecyclerViewAdapter;
+import com.train2gain.train2gain.adapter.model.RecyclerViewItem;
+import com.train2gain.train2gain.model.entity.ScheduleDailyWorkout;
+import com.train2gain.train2gain.model.entity.ScheduleStep;
+import com.train2gain.train2gain.model.enums.MuscleGroup;
+import com.train2gain.train2gain.viewmodel.ScheduleDailyWorkoutViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DailyWorkoutFragment extends Fragment {
 
     private final static String ATHLETE_USER_ID_KEY = "ATHLETE_USER_ID_KEY";
+
     private long athleteUserId = -1;
+    private final DailyWorkoutRecyclerViewAdapter dailyWorkoutRecyclerViewAdapter;
 
     public static DailyWorkoutFragment newInstance(final long athleteUserId){
         DailyWorkoutFragment dailyWorkoutFragment = new DailyWorkoutFragment();
@@ -19,6 +34,10 @@ public class DailyWorkoutFragment extends Fragment {
         fragmentArgs.putLong(DailyWorkoutFragment.ATHLETE_USER_ID_KEY, athleteUserId);
         dailyWorkoutFragment.setArguments(fragmentArgs);
         return dailyWorkoutFragment;
+    }
+
+    public DailyWorkoutFragment(){
+        this.dailyWorkoutRecyclerViewAdapter = new DailyWorkoutRecyclerViewAdapter();
     }
 
     @Override
@@ -34,13 +53,27 @@ public class DailyWorkoutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_daily_workout_athlete, parent, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_daily_workout_athlete, parent, false);
+        RecyclerView recyclerView = (RecyclerView) fragmentView.findViewById(R.id.daily_workout_schedule_steps_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(this.dailyWorkoutRecyclerViewAdapter);
+        recyclerView.setHasFixedSize(true);
+        return fragmentView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        // TODO add view model managing
+
+        // Get dailyWorkout and update the recyclerView
+        ScheduleDailyWorkoutViewModel scheduleDailyWorkoutViewModel = ViewModelProviders.of(this).get(ScheduleDailyWorkoutViewModel.class);
+        scheduleDailyWorkoutViewModel.getScheduleDailyWorkoutOfTheDay(this.athleteUserId).observe(this, scheduleDailyWorkoutResource -> {
+            if(scheduleDailyWorkoutResource != null && scheduleDailyWorkoutResource.getData() != null){
+                List<RecyclerViewItem> recyclerViewRecyclerViewItems = convertToRecyclerViewItems(scheduleDailyWorkoutResource.getData());
+                this.dailyWorkoutRecyclerViewAdapter.setNewRecyclerViewItemList(recyclerViewRecyclerViewItems);
+                this.dailyWorkoutRecyclerViewAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -52,6 +85,30 @@ public class DailyWorkoutFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Nothing to do
+    }
+
+    /**
+     * Prepares the list of items that will be displayed in the daily workout's recycler view
+     * @param scheduleDailyWorkout the object that contains the list of items that we want to display in the recycler view
+     * @return the list of items that will be displayed in the daily workout's recycler view
+     */
+    @NonNull
+    private List<RecyclerViewItem> convertToRecyclerViewItems(@NonNull final ScheduleDailyWorkout scheduleDailyWorkout){
+        List<RecyclerViewItem> recyclerViewRecyclerViewItems = new ArrayList<RecyclerViewItem>();
+
+        // Init Recycler view items list
+        List<ScheduleStep> scheduleStepList = scheduleDailyWorkout.getScheduleStepList();
+        MuscleGroup muscleGroup = null;
+        for(ScheduleStep scheduleStep : scheduleStepList){
+            MuscleGroup muscleGroupTemp = scheduleStep.getScheduleSetList().get(0).getScheduleSetItemList().get(0).getExercise().getMuscleGroup();
+            if(muscleGroup == null || muscleGroupTemp.getKey() != muscleGroup.getKey()){
+                muscleGroup = muscleGroupTemp;
+                recyclerViewRecyclerViewItems.add(new RecyclerViewItem<String>(RecyclerViewItem.ItemType.HEADER, muscleGroup.toString()));
+            }
+            recyclerViewRecyclerViewItems.add(new RecyclerViewItem<ScheduleStep>(RecyclerViewItem.ItemType.STANDARD_SET, scheduleStep));
+        }
+
+        return recyclerViewRecyclerViewItems;
     }
 
 }
